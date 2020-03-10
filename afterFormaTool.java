c9b6ad5d16b58e3d6c8db3ce8738fca837d43165
Zacharias Vlatakis <zvracy13@gmail.com>
2020-03-10 19:19:44 +0200

    Create afterFormaTool.java

---
+++ afterFormaTool.java
@@ -0,0 +1,29 @@
+package test;
+import java.io.IOException;
+public class Test {
+public static void main(String[] args) throws IOException {
+System.out.println("Welcome to my AfterFormat Tool!");
+String url_open ="https://github.com/Sycnex/Windows10Debloater";
+java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open));
+String url_open1 ="https://d38w655bqoyvyi.cloudfront.net/manager/SteamGridDB+Manager+Setup+0.4.2.exe";
+java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open1));
+String url_open2 ="http://www.motospeed.cc/upfile/download/MotoSpeed_GamingMouse_V30(20180627).rar";
+java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open2));
+String url_open3 ="https://www.ccleaner.com/speccy/download/standard";
+java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open3));
+String url_open4 ="https://www.unifiedremote.com/download/windows";
+java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open4));
+String url_open5 ="https://app.prntscr.com/build/setup-lightshot.exe";
+java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open5));
+String url_open6 ="https://www.blizzard.com/en-us/apps/battle.net/desktop";
+java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open6));
+String url_open7 ="https://get.popcorntime.sh/build/Popcorn-Time-0.3.10-Setup.exe";
+java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open7));
+String url_open8 ="https://laptop-updates.brave.com/latest/winx64";
+java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open8));
+String url_open9 ="https://download.onlyoffice.com/install/desktop/editors/windows/distrib/onlyoffice/DesktopEditors_x64.exe";
+java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open9));
+String url_open10 ="https://github.com/Sycnex/Windows10Debloater/archive/master.zip";
+java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open10));
+}
+}
