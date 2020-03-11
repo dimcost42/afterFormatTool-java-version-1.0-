12866250ec6a56df37e43b97580c63811036fa83
Zacharias Vlatakis <zvracy13@gmail.com>
2020-03-11 19:11:20 +0200

    Add files via upload

---
+++ format_tool.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+:: THIS BAT FILE DOWNLOADS ALL YOUR FAVORITE SOFTWARE WITH THE PRESS OF A BUTTON!
+TITLE AFTER-FORMAT TOOL
+
+start http://www.motospeed.cc/upfile/download/MotoSpeed_GamingMouse_V30(20180627).rar
+start https://github.com/Sycnex/Windows10Debloater/archive/master.zip
+start https://d38w655bqoyvyi.cloudfront.net/manager/SteamGridDB+Manager+Setup+0.4.2.exe
+start https://www.ccleaner.com/speccy/download/standard
+start https://www.unifiedremote.com/download/windows
+start https://app.prntscr.com/build/setup-lightshot.exe
+start https://www.blizzard.com/en-us/apps/battle.net/desktop
+start https://github.com/Nexus-Mods/Nexus-Mod-Manager/releases/download/0.71.2/Nexus.Mod.Manager-0.71.2.exe
+start https://get.popcorntime.sh/build/Popcorn-Time-0.3.10-Setup.exe
+start https://laptop-updates.brave.com/latest/winx64
+start https://download.onlyoffice.com/install/desktop/editors/windows/distrib/onlyoffice/DesktopEditors_x64.exe
+
+@ECHO OFF
+ECHO Grats kid your apps are here!
+PAUSE
